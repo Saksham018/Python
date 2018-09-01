36d6161dc6de4ea4b8644089e4e59f71c71bae8e
saksham018 <sakshamkapila018@gmail.com>
2018-09-01 10:53:13 +0530

    done

---
+++ Ass-12.py
@@ -0,0 +1,11 @@
+#Question 1
+from datetime import date
+days=['Monday','Tuesday','Wednessday','Thursday','Friday','Saturday','Sunday']
+print(days[date.weekday(date.today())])
+#Question 2
+import webbrowser
+webbrowser.open('https://www.youtube.com/watch?v=DUMb9fxWrN4')
+#Question 3
+import os
+os.rename('file.txt','file.jpg')
+
